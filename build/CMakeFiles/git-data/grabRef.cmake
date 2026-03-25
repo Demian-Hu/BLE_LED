910eed3b3ece370d8155ea9db8f3a125c0976cf7
Demian-Hu <2091432901@qq.com>
2026-03-25 20:05:30 +0800

    在项目组件中添加了LVGL9.2.2,为之后的GUI开发准备

--- build/CMakeFiles/git-data/grabRef.cmake
+++ build/CMakeFiles/git-data/grabRef.cmake
@@ -18,7 +18,7 @@ set(HEAD_HASH)
 file(READ "E:/esp/remake/BLE_LED/build/CMakeFiles/git-data/HEAD" HEAD_CONTENTS LIMIT 1024)
 
 string(STRIP "${HEAD_CONTENTS}" HEAD_CONTENTS)
-set(GIT_DIR "E:/esp/v5.5.3/esp-idf/.git")
+set(GIT_DIR "E:/esp/remake/BLE_LED/.git")
 # handle git-worktree
 if(EXISTS "${GIT_DIR}/commondir")
 	file(READ "${GIT_DIR}/commondir" GIT_DIR_NEW LIMIT 1024)
--- build/esp-idf/cmake_install.cmake
+++ build/esp-idf/cmake_install.cmake
@@ -572,3 +572,8 @@ if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   include("E:/esp/remake/BLE_LED/build/esp-idf/main/cmake_install.cmake")
 endif()
 
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("E:/esp/remake/BLE_LED/build/esp-idf/lvgl/cmake_install.cmake")
+endif()
+
